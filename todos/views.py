b180c2680d6af52e51b9cfa473a8f0f98b951249
Gabriel Moscarde <gabrielmoscarde@outlook.com>
2025-01-11 16:09:29 -0300

    feat: add update and delete functionality for todos

--- todos/views.py
+++ todos/views.py
@@ -1,18 +1,26 @@
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.views.generic import CreateView, DeleteView, ListView, UpdateView
+
 from .models import Todo
 
-
-# def todo_list(request):
-#     todos = Todo.objects.all()
-
-#     return render(request, "todos/todo_list.html", {"todos": todos})
 
 class TodoListView(ListView):
     model = Todo
     template_name = "todos/todo_list.html"
 
+
 class TodoCreateView(CreateView):
     model = Todo
     fields = ["title", "deadline"]
+    success_url = reverse_lazy("todo_list")
+
+
+class TodoUpdateView(UpdateView):
+    model = Todo
+    fields = ["title", "deadline"]
+    success_url = reverse_lazy("todo_list")
+
+
+class TodoDeleteView(DeleteView):
+    model = Todo
     success_url = reverse_lazy("todo_list")
